d86f5ffdcd0992aaf660e13b10345851aa814053
Prashant Godhwani <prashant.godhwani@encodezero.com>
2018-08-05 10:46:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+  var log = `${new Date().toString()} : ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log(err);
+  }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintaince.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs',{
+    pageTitle : 'Home',
+    bodyText : "This text should come here"
+  });
+});
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs',{
+    pageTitle : 'About',
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    status : 0,
+    errorMessage : 'Request Failed.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
